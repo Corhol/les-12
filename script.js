5e8773082ca6674718998e77447e570980bb14ad
Sasisonisik <tolkacigor485@gmail.com>
2026-01-17 15:23:51 +0200

    da

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const openButton = document.querySelector(".openbut");
 const closeButton = document.querySelector(".closebut");
 const backgrod = document.querySelector(".backgrod");
+const modal = document.querySelector(".modal");
 openButton.addEventListener("click", function () {
   backgrod.classList.remove("hidden");
+  setTimeout(function () {
+    backgrod.classList.add("visible");
+    modal.classList.add("modalVisual");
+  }, 10);
 });
 closeButton.addEventListener("click", function () {
-  backgrod.classList.add("hidden");
+  backgrod.classList.remove("visible");
+  modal.classList.remove("modalVisual");
+  setTimeout(function () {
+    backgrod.classList.add("hidden");
+  }, 300);
 });
